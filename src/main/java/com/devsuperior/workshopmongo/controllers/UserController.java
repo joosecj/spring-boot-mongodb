c4df9b3346c57215e6a67ad49db1863961195cab
Jose Carlos <josecarloscjj@gmail.com>
2022-11-14 10:50:02 -0300

    Inserindo usuario com POST

--- src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
@@ -7,10 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -27,5 +27,13 @@ public class UserController {
     @RequestMapping(value ="/{id}", method = RequestMethod.GET)
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         return ResponseEntity.ok(userService.findById(id));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<UserDTO> inset(@RequestBody UserDTO userDTO) {
+        userDTO = userService.insert(userDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(userDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(userDTO);
     }
 }
--- src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -8,20 +8,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
-
+    @Transactional(readOnly = true)
     public Page<UserDTO> findAll(Pageable pageable) {
         Page<User> userPage = userRepository.findAll(pageable);
         return userPage.map(UserDTO::new);
     }
-
+    @Transactional(readOnly = true)
     public UserDTO findById(String id) {
         User userEntity = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado"));
         return new UserDTO(userEntity);
+    }
+
+    @Transactional(readOnly = false)
+    public UserDTO insert(UserDTO userDTO) {
+        User userEntity = new User();
+        copyDtoToEntity(userDTO, userEntity);
+        userRepository.save(userEntity);
+        return new UserDTO(userEntity);
+    }
+
+    private void copyDtoToEntity(UserDTO userDTO, User userEntity) {
+        userEntity.setName(userDTO.getName());
+        userEntity.setEmail(userDTO.getEmail());
     }
 }
