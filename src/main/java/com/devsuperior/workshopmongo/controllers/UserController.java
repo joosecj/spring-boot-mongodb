491536bf914c75143d9797168f96b6736a52f9fe
Jose Carlos <josecarloscjj@gmail.com>
2022-11-14 18:45:38 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.devsuperior.workshopmongo.controllers;
 
 import com.devsuperior.workshopmongo.dto.UserDTO;
 
+import com.devsuperior.workshopmongo.entities.Post;
+import com.devsuperior.workshopmongo.entities.User;
 import com.devsuperior.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -45,5 +48,11 @@ public class UserController {
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public ResponseEntity<UserDTO> update(@PathVariable String id, @RequestBody UserDTO userDTO) {
         return ResponseEntity.ok().body(userService.update(id, userDTO));
+    }
+
+    @RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = userService.findByPosts(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
--- src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -3,16 +3,15 @@ package com.devsuperior.workshopmongo.services;
 import com.devsuperior.workshopmongo.dto.UserDTO;
 import com.devsuperior.workshopmongo.entities.User;
 import com.devsuperior.workshopmongo.repositories.UserRepository;
-import com.devsuperior.workshopmongo.services.exeptions.DataBaseException;
 import com.devsuperior.workshopmongo.services.exeptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -50,6 +49,12 @@ public class UserService {
         User userEntity = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado"));
         copyDtoToEntity(userDTO, userEntity);
         return new UserDTO(userRepository.save(userEntity));
+    }
+
+    @Transactional(readOnly = true)
+    public User findByPosts(String id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
     }
 
     private void copyDtoToEntity(UserDTO userDTO, User userEntity) {
